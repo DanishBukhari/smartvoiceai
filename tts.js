7a80c6d27734f62ce883337540e51e20d8dfcd0a
DanishBukhari <dani.syed.18@gmail.com>
2025-06-21 16:58:43 +1000

    fdad

--- tts.js
+++ tts.js
@@ -10,7 +10,12 @@ async function streamTTS(req, res) {
   const text = req.query.text || "";
   const voiceId = "LXy8KWda5yk1Vw6sEV6w";
 
-  
+    const postData = JSON.stringify({
+    text,
+    model_id: "eleven_flash_v2.5",
+    voice_settings: { stability: 0.5, similarity_boost: 0.75 },
+  });
+
   const options = {
     hostname: "api.elevenlabs.io",
     path: `/v1/text-to-speech/${voiceId}/stream`,
@@ -19,42 +24,15 @@ async function streamTTS(req, res) {
       "xi-api-key": process.env.ELEVENLABS_API_KEY,
       "Content-Type": "application/json",
       "Accept": "audio/mpeg",
-      // 'Content-Length': Buffer.byteLength(postData),
+      'Content-Length': Buffer.byteLength(postData),
     },
   };
-  const postData = JSON.stringify({
-  text,
-  model_id: "eleven_flash_v2.5",
-  voice_settings: { stability: 0.5, similarity_boost: 0.75 },
-});
 
   const request = https.request(options, (response) => {
     res.setHeader("Content-Type", "audio/mpeg");
     res.setHeader("Transfer-Encoding", "chunked");
-    // res.setHeader('Content-Length', audioBuffer.length);
     response.pipe(res);
   });
-
-  // const chunks = [];
-  // const reqEle = https.request(options, (eleRes) => {
-  //   eleRes.on('data', (chunk) => chunks.push(chunk));
-  //   eleRes.on('end', () => {
-  //     const audioBuffer = Buffer.concat(chunks);
-  //     res.setHeader('Content-Type', 'audio/mpeg');
-  //     res.setHeader('Content-Length', audioBuffer.length);
-  //     res.end(audioBuffer);
-  //   });
-  // });
-
-  
-//   reqEle.on('error', (err) => {
-//     console.error('TTS request error:', err);
-//     res.status(500).end();
-//   });
-
-//   reqEle.write(postData);
-//   reqEle.end();
-// }
 
   request.on("error", (err) => {
     console.error("Request error:", err.message);
